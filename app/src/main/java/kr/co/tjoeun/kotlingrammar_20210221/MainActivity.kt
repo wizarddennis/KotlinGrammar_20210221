ba163c55fc2ed636c0e9eb9ee368a4011ac18c09
dennis <travlInfo4you@gmail.com>
2021-02-21 09:50:18 +0900

    var /val 사용 예시

--- app/src/main/java/kr/co/tjoeun/kotlingrammar_20210221/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/kotlingrammar_20210221/MainActivity.kt
@@ -7,5 +7,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 변수로 => 정수를 담을 공간. JAVA : int num1;
+        var num1 : Int
+
+        num1 = 10
+        num1 = 20
+
+        //상수로 => 정수를 담을 공간
+        val num2 : Int
+        num2 = 10
+        //num2 = 20. 한번 대입하고 나면 변경 불가
+
     }
 }
